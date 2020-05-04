5773b474067de0630e875444bc0af083da5768ed
joep <joep.stevens@student.uhasselt.be>
2020-05-04 17:38:27 +0200

    product view logic update

--- project_code/app/src/main/java/com/example/hsai_project/fragments/ProductViewFragment.java
+++ project_code/app/src/main/java/com/example/hsai_project/fragments/ProductViewFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.hsai_project.R;
 
@@ -15,6 +16,8 @@ import com.example.hsai_project.R;
 public class ProductViewFragment extends Fragment {
 
     private boolean m_fav = false;
+    private TextView m_name;
+    private TextView m_price;
 
     public ProductViewFragment() {
         // Required empty public constructor
@@ -31,18 +34,41 @@ public class ProductViewFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_product_view, container, false);
+        // favorite button
         ImageView fav = (ImageView) root.findViewById(R.id.product_fav_button);
         fav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ImageView view = ((ImageView)v);
-                m_fav = !m_fav;
-                if(m_fav)
-                    view.setImageResource(R.drawable.ic_favorite_black_24dp);
-                else
-                    view.setImageResource(R.drawable.ic_favorite_border_black_24dp);
+                toggleFavorite(v);
             }
         });
+
+        m_name = (TextView) root.findViewById(R.id.product_text);
+        // TODO set name
+        m_price = (TextView) root.findViewById(R.id.product_price);
+        // TODO set price
+
+
         return root;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // check update name and price from database
+    }
+
+    private void toggleFavorite(View v){
+        ImageView view = ((ImageView)v);
+        m_fav = !m_fav;
+        if(m_fav)
+            view.setImageResource(R.drawable.ic_favorite_black_24dp);
+        else
+            view.setImageResource(R.drawable.ic_favorite_border_black_24dp);
+    }
+
+    private void onClick(){
+
+    }
+
 }
