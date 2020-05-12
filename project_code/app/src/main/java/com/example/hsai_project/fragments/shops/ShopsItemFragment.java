c5ec7b6c7565e31c7bb07a11715a62bf3080a568
joep <joep.stevens@student.uhasselt.be>
2020-05-12 16:38:44 +0200

    winkellist

--- project_code/app/src/main/java/com/example/hsai_project/fragments/shops/ShopsItemFragment.java
+++ project_code/app/src/main/java/com/example/hsai_project/fragments/shops/ShopsItemFragment.java
@@ -3,12 +3,16 @@ package com.example.hsai_project.fragments.shops;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.hsai_project.ProductDatabase;
+import com.example.hsai_project.ProductEntity;
 import com.example.hsai_project.R;
+import com.example.hsai_project.fragments.explore.ExploreScrollFragment;
 
 
 public class ShopsItemFragment extends Fragment {
@@ -21,7 +25,16 @@ public class ShopsItemFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_shops_item, container, false);
+        final View root = inflater.inflate(R.layout.fragment_shops_item, container, false);
+
+        root.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putInt("StoreId", 0);
+                Navigation.findNavController(root).navigate(R.id.store_fragment, bundle);
+            }
+        });
 
         return root;
     }
